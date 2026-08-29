b72f72fd3de66e5792bd2995b9f531fa5e21448a
jefazo1983 <gfflores11@gmail.com>
2026-08-28 20:46:57 -0400

    Update channels.js

--- channels.js
+++ channels.js
@@ -1,42 +1,42 @@
 const channels = [
     {
-        "name": "DSports",
-        "logo": "https://upload.wikimedia.org/wikipedia/commons/4/45/Directv_Sports_logo_2020.svg",
-        "url": "https://98.domhsd.com/dsports/tracks-v1a1/mono.m3u8?ip=181.115.161.231&token=584e5a4639c4430a2a034e32236d88efa1402880-32-1787998471-1787944471"
+        name: "DSports",
+        type: "dsports",
+        url: "https://98.domhsd.com/dsports/tracks-v1a1/mono.m3u8?ip=181.115.161.231&token=584e5a4639c4430a2a034e32236d88efa1402880-32-1787998471-1787944471"
     },
     {
-        "name": "DSports 2",
-        "logo": "https://upload.wikimedia.org/wikipedia/commons/4/45/Directv_Sports_logo_2020.svg",
-        "url": "https://193.domhsd.com/dsports2/tracks-v1a1/mono.m3u8?ip=181.115.161.231&token=a366f4a2b5e5a2b55ab5970268d2e9e6ffb3b4a3-6c-1787998479-1787944479"
+        name: "DSports 2",
+        type: "dsports2",
+        url: "https://193.domhsd.com/dsports2/tracks-v1a1/mono.m3u8?ip=181.115.161.231&token=a366f4a2b5e5a2b55ab5970268d2e9e6ffb3b4a3-6c-1787998479-1787944479"
     },
     {
-        "name": "DSports +",
-        "logo": "https://upload.wikimedia.org/wikipedia/commons/4/45/Directv_Sports_logo_2020.svg",
-        "url": "https://47.domhsd.com/dsportsplus/tracks-v1a1/mono.m3u8?ip=181.115.161.231&token=9ea0f4d474482b96bdb6c314ffb0c40c7d2e115e-fe-1787998485-1787944485"
+        name: "DSports +",
+        type: "dsportsplus",
+        url: "https://47.domhsd.com/dsportsplus/tracks-v1a1/mono.m3u8?ip=181.115.161.231&token=9ea0f4d474482b96bdb6c314ffb0c40c7d2e115e-fe-1787998485-1787944485"
     },
     {
-        "name": "ESPN",
-        "logo": "https://upload.wikimedia.org/wikipedia/commons/2/2a/ESPN_logo.svg",
-        "url": "https://98.domhsd.com/espn/tracks-v1a1/mono.m3u8?ip=181.115.161.231&token=086c9ad5fa41d7adc66a886973d453bb1eb2e3f3-92-1787998492-1787944492"
+        name: "ESPN",
+        type: "espn",
+        url: "https://98.domhsd.com/espn/tracks-v1a1/mono.m3u8?ip=181.115.161.231&token=086c9ad5fa41d7adc66a886973d453bb1eb2e3f3-92-1787998492-1787944492"
     },
     {
-        "name": "ESPN 2",
-        "logo": "https://upload.wikimedia.org/wikipedia/commons/2/2a/ESPN_logo.svg",
-        "url": "https://93.domhsd.com/espn2/tracks-v1a1/mono.m3u8?ip=181.115.161.231&token=aa20d195d46e286d9b5b34fe691094ead3a03592-1f-1787998498-1787944498"
+        name: "ESPN 2",
+        type: "espn2",
+        url: "https://93.domhsd.com/espn2/tracks-v1a1/mono.m3u8?ip=181.115.161.231&token=aa20d195d46e286d9b5b34fe691094ead3a03592-1f-1787998498-1787944498"
     },
     {
-        "name": "ESPN 3",
-        "logo": "https://upload.wikimedia.org/wikipedia/commons/2/2a/ESPN_logo.svg",
-        "url": "https://193.domhsd.com/espn3/tracks-v1a1/mono.m3u8?ip=181.115.161.231&token=f9e0e10a0afb813dcd8a8cb7ab78c3055f535bff-b6-1787998505-1787944505"
+        name: "ESPN 3",
+        type: "espn3",
+        url: "https://193.domhsd.com/espn3/tracks-v1a1/mono.m3u8?ip=181.115.161.231&token=f9e0e10a0afb813dcd8a8cb7ab78c3055f535bff-b6-1787998505-1787944505"
     },
     {
-        "name": "ESPN Premium",
-        "logo": "https://upload.wikimedia.org/wikipedia/commons/2/2a/ESPN_logo.svg",
-        "url": "https://98.domhsd.com/espnpremium/tracks-v1a1/mono.m3u8?ip=181.115.161.231&token=6ed55937e19a7462ac343d7ad66ac2206ab1fd37-6b-1787998511-1787944511"
+        name: "ESPN Premium",
+        type: "espnprem",
+        url: "https://98.domhsd.com/espnpremium/tracks-v1a1/mono.m3u8?ip=181.115.161.231&token=6ed55937e19a7462ac343d7ad66ac2206ab1fd37-6b-1787998511-1787944511"
     },
     {
-        "name": "TNT Sports",
-        "logo": "https://upload.wikimedia.org/wikipedia/commons/3/3b/TNT_Sports_logo_2017.svg",
-        "url": "https://7.domhsd.com/tntsports/tracks-v1a1/mono.m3u8?ip=181.115.161.231&token=9852856b64fdab7d2cc5ed608a64dd4adc491d09-c3-1787998518-1787944518"
+        name: "TNT Sports",
+        type: "tnt",
+        url: "https://7.domhsd.com/tntsports/tracks-v1a1/mono.m3u8?ip=181.115.161.231&token=9852856b64fdab7d2cc5ed608a64dd4adc491d09-c3-1787998518-1787944418"
     }
 ];
